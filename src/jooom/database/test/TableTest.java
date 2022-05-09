43abae3b2cd083cd7dc053cd41843a3485dc6f56
jooomOut <rimeilo324@naver.com>
2022-05-09 15:22:49 +0900

    Test: 테스트 기본 구조 생성
    Main은 Application으로 옮기고 테스트 시작만 관리, 세부 내용은 몰라야 한다

---
+++ src/jooom/database/test/TableTest.java
@@ -0,0 +1,13 @@
+package jooom.database.test;
+
+import jooom.database.DatabaseInterface;
+
+public class TableTest {
+    private DatabaseInterface databaseInterface;
+
+    public TableTest(DatabaseInterface databaseInterface) {
+        this.databaseInterface = databaseInterface;
+    }
+
+    // TODO: 테이블 생성 테스트하기
+}
---
+++ src/jooom/database/test/TestManager.java
@@ -0,0 +1,15 @@
+package jooom.database.test;
+
+import jooom.database.DatabaseInterface;
+import jooom.database.DatabaseInterfaceImpl;
+
+public class TestManager {
+    private DatabaseInterface databaseInterface = new DatabaseInterfaceImpl();
+
+    public TestManager() {
+    }
+
+    public void run(){
+
+    }
+}
