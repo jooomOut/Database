d95a0c9047a73154cd6a32e9188e839285643b25
jooomOut <rimeilo324@naver.com>
2022-05-14 21:05:34 +0900

    Fix: 레코드 검색 타입 변경

--- src/jooom/database/main/DatabaseInterface.java
+++ src/jooom/database/main/DatabaseInterface.java
@@ -1,13 +1,13 @@
-package jooom.database;
+package jooom.database.main;
 
-import jooom.database.dto.TableDto;
+import jooom.database.main.dto.TableDto;
 
 import java.io.IOException;
 import java.util.Map;
 
 public interface DatabaseInterface {
     void createTable(TableDto tableDto) throws IOException;
-    void insert(String tableName, Map<String, Object> columns);
-    Map<String, Object> search(String key);
+    void insert(String tableName, Map<String, String> columns);
+    Map<String, String> search(String key);
     String[] search(String columnName, String key);
 }
---
+++ src/jooom/database/main/DatabaseInterfaceImpl.java
@@ -0,0 +1,36 @@
+package jooom.database.main;
+
+import jooom.database.main.dto.TableDto;
+import jooom.database.main.service.TableManager;
+import jooom.database.main.service.impl.TableManagerImpl;
+
+
+import java.io.IOException;
+import java.util.Map;
+
+public class DatabaseInterfaceImpl implements DatabaseInterface {
+    private TableManager tableManager = new TableManagerImpl();
+    @Override
+    public void createTable(TableDto tableDto) throws IOException{
+        tableManager.createTable(tableDto);
+    }
+
+    @Override
+    public Map<String, String> search(String key) {
+        return null;
+    }
+
+    @Override
+    public String[] search(String columnName, String key) {
+        return new String[0];
+    }
+
+    @Override
+    public void insert(String tableName, Map<String, String> columns) {
+        // todo: 1. 테이블 데이터 읽어서 순서 맞추기
+        // TODO : 2. 데이터 추가하기
+        // TODO : 3. 데이터 추가하기
+        // TODO : 2. 데이터 추가하기
+
+    }
+}
