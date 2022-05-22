650bfdb8637af33be4f585f8514ee7031627b360
jooomOut <rimeilo324@naver.com>
2022-05-22 13:23:14 +0900

    Fix: 인터페이스에 필드 추가

--- src/jooom/database/main/record/structure/RecordStructure.java
+++ src/jooom/database/main/record/structure/RecordStructure.java
@@ -1,11 +1,13 @@
 package jooom.database.main.record.structure;
 
+import jooom.database.main.service.TableManager;
+
 import java.util.Map;
 
 abstract public class RecordStructure {
-
-    public RecordStructure() {
-
+    protected final TableManager tableManager;
+    public RecordStructure(TableManager tableManager) {
+        this.tableManager = tableManager;
     }
 
     abstract public byte[] toByteFrom(String tableName, Map<String, String> columns);
